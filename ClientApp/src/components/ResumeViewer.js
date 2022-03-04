dcea50e4b1b32dba9de167b0da5cef6bfc22d68a
Alexiel <alexielm@rogers.com>
2022-03-04 15:14:48 -0500

    Adding Chart Dialog

--- ClientApp/src/components/ResumeViewer.js
+++ ClientApp/src/components/ResumeViewer.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import { Popover } from 'antd';
+import React, { Component, Fragment } from 'react';
+import { Popover, Modal } from 'antd';
 import "./ResumeViewer.css";
 
 
 import PhoneIcon from './images/phone.svg';
 import EmailIcon from './images/email.svg';
-import PrinterIcon from './images/printer.svg';
+import PrintIcon from './images/print.svg';
+import ChartIcon from './images/chart.svg';
+
 import App from '../App';
 import { RemoveHttp } from '../generalUtils/Utils';
 import { PersonalRemarks } from './PersonalRemarks';
@@ -13,6 +15,7 @@ import { Topic } from './Topic';
 import { TechnicalSkillsList } from './TechnicalSkillsList';
 import { JobsEventsList } from './JobsEventsList';
 import { EducationEventsList } from './EducationEventsList';
+import { SkillsChart } from './SkillsChart';
 
 export class ResumeViewer extends Component {
     static displayName = ResumeViewer.name;
@@ -22,6 +25,17 @@ export class ResumeViewer extends Component {
 
         this.refreshPage = this.refreshPage.bind(this);
         this.printPage = this.printPage.bind(this);
+
+        this.openSkillsChart = this.openSkillsChart.bind(this);
+        this.closeSkillsChart = this.closeSkillsChart.bind(this);
+
+        this.openExperienceChart = this.openExperienceChart.bind(this);
+        this.closeExperienceChart = this.closeExperienceChart.bind(this);
+
+        this.state = {
+            skillsChartVisible: true,
+            experienceChartVisible: false
+        }
     }
 
     componentDidMount() {
@@ -47,6 +61,30 @@ export class ResumeViewer extends Component {
         window.print();
     }
 
+    openSkillsChart() {
+        this.setState({
+            skillsChartVisible: true
+        });
+    }
+
+    closeSkillsChart() {
+        this.setState({
+            skillsChartVisible: false
+        });
+    }
+
+    openExperienceChart() {
+        this.setState({
+            experienceChartVisible: true
+        });
+    }
+
+    closeExperienceChart() {
+        this.setState({
+            experienceChartVisible: false
+        });
+    }
+
     render() {
         let resumeData = App.ResumeData;
         if (resumeData === null) {
@@ -56,44 +94,79 @@ export class ResumeViewer extends Component {
         }
         let contact = resumeData.contact;
         return (
-            <div className="ResumeViewerBody MainContent">
-                <div className="TopFadeout"></div>
-                <div>
-                    <div className="AboutPerson KeepTogether">
-                        <div className="Personal">
-                            <span className="FullName">{resumeData.firstName}&nbsp;{resumeData.lastName}</span>
-                            <Popover placement="right" content="Click to print">
-                                <img src={PrinterIcon} className="PrintIcon" alt="print" onClick={this.printPage} />
-                            </Popover>
-
-                            <div className="ContactInfo">
-                                <div><img src={PhoneIcon} className="SmallIcon" alt="phone" />&nbsp;{contact.phone}</div>
-                                <div><img src={EmailIcon} className="SmallIcon" alt="email" onDoubleClick={this.refreshPage} />&nbsp;<a href={`mailto:${contact.email}`}>{contact.email}</a></div>
+            <Fragment>
+                <div className="ResumeViewerBody MainContent">
+                    <div className="TopFadeout"></div>
+                    <div>
+                        <div className="AboutPerson KeepTogether">
+                            <div className="Personal">
+                                <span className="FullName">{resumeData.firstName}&nbsp;{resumeData.lastName}</span>
+                                <Popover placement="right" content="Click to print">
+                                    <img src={PrintIcon} className="PrintIcon" alt="print" onClick={this.printPage} />
+                                </Popover>
+                                <div className="ContactInfo">
+                                    <div><img src={PhoneIcon} className="SmallIcon" alt="phone" />&nbsp;{contact.phone}</div>
+                                    <div><img src={EmailIcon} className="SmallIcon" alt="email" onDoubleClick={this.refreshPage} />&nbsp;<a href={`mailto:${contact.email}`}>{contact.email}</a></div>
+                                </div>
+                            </div>
+                            <div className="Links">
+                                {
+                                    Object
+                                        .entries(contact.links)
+                                        .map(([name, url]) => <span key={name} className="WebLink">{name}: <a href={url} rel="noreferrer" target="_blank">{RemoveHttp(url)}</a></span>)
+                                }
                             </div>
                         </div>
-                        <div className="Links">
-                            {
-                                Object
-                                    .entries(contact.links)
-                                    .map(([name, url]) => <span key={name} className="WebLink">{name}: <a href={url} rel="noreferrer" target="_blank">{RemoveHttp(url)}</a></span>)
-                            }
-                        </div>
+                        <Topic title="REMARKS" className="KeepTogether">
+                            <PersonalRemarks remarks={resumeData.remarks} />
+                        </Topic>
+                        <Topic
+                            title={<>
+                                TECHNICAL SKILLS
+                                <Popover placement="right" content="Click to view skills chart">
+                                    <img src={ChartIcon} className="ChartIcon" alt="chart" onClick={this.openSkillsChart} />
+                                </Popover>
+                            </>}
+                            className="KeepTogether"
+                        >
+                            <TechnicalSkillsList timeLine={resumeData.timeLine} />
+                        </Topic>
+                        <Topic
+                            title={<>
+                                WORK EXPERIENCE
+                                <Popover placement="right" content="Click to view experience chart">
+                                    <img src={ChartIcon} className="ChartIcon" alt="chart" onClick={this.openExperienceChart} />
+                                </Popover>
+                            </>}
+                        >
+                            <JobsEventsList timeLine={resumeData.timeLine} />
+                        </Topic>
+                        <Topic title="EDUCATION" className="KeepTogether">
+                            <EducationEventsList timeLine={resumeData.timeLine} />
+                        </Topic>
                     </div>
-                    <Topic title="REMARKS" className="KeepTogether">
-                        <PersonalRemarks remarks={resumeData.remarks} />
-                    </Topic>
-                    <Topic title="TECHNICAL SKILLS" className="KeepTogether">
-                        <TechnicalSkillsList timeLine={resumeData.timeLine} />
-                    </Topic>
-                    <Topic title="WORK EXPERIENCE">
-                        <JobsEventsList timeLine={resumeData.timeLine} />
-                    </Topic>
-                    <Topic title="EDUCATION" className="KeepTogether">
-                        <EducationEventsList timeLine={resumeData.timeLine} />
-                    </Topic>
+                    <div className="BottomFadeout"></div>
                 </div>
-                <div className="BottomFadeout"></div>
-            </div>
+                <Modal
+                    visible={this.state.skillsChartVisible}
+                    title="Technical Skills"
+                    width={1040}
+                    okButtonProps={{
+                        style: {
+                            display: "none"
+                        }
+                    }}
+
+
+                    onCancel={this.closeSkillsChart}
+                    cancelText="Close"
+                    cancelButtonProps={{
+                        type: "default"
+                    }}
+                >
+                    <SkillsChart />
+                </Modal>
+            </Fragment>
         );
     }
 }
--- ClientApp/src/components/SkillsChart.js
+++ ClientApp/src/components/SkillsChart.js
@@ -9,27 +9,23 @@ import {
 } from 'recharts';
 
 
-export class Chart extends Component {
-    static displayName = Chart.name;
+export class SkillsChart extends Component {
+    static displayName = SkillsChart.name;
 
     data = [
-        { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
-        { name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
-        { name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
-        { name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
-        { name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28 },
-        { name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
-        { name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
-        { name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
-        { name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
-        { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
+        { name: '1995', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
+        { name: '2000', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
+        { name: '2005', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
+        { name: '2010', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
+        { name: '2015', uv: 278, pv: null, amt: 2400, uvError: 28 },
+        { name: '2020', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] }
     ];
 
     render() {
         return (
             <div>
                 <LineChart
-                    width={1200}
+                    width={1000}
                     height={400}
                     data={this.data}
                     margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
